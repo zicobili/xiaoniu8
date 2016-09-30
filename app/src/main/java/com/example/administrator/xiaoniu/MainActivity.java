4d40a24db7f1b0c8124f20e763ec041fcc8e7410
1505737818@qq.com <1505737818@qq.com>
2016-09-30 11:06:14 +0800

    新建底部导航

--- app/src/main/java/com/example/administrator/xiaoniu/MainActivity.java
+++ app/src/main/java/com/example/administrator/xiaoniu/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.administrator.xiaoniu;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+
+    private RadioGroup rg_tab_bar;
+    private RadioButton rb_home;
+
+    //Fragment Object
+    private FragmentHome fg1;
+    private FragmentContact fg2;
+    private FragmentMy fg3;
+
+    private FragmentManager fManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_userinfo);
+        setContentView(R.layout.activity_main);
+        fManager = getFragmentManager();
+        rg_tab_bar = (RadioGroup) findViewById(R.id.rbg_main_content);
+        rg_tab_bar.setOnCheckedChangeListener(this);
+        //获取第一个单选按钮，并设置其为选中状态
+        rb_home = (RadioButton) findViewById(R.id.rb_home);
+        rb_home.setChecked(true);
     }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentTransaction fTransaction = fManager.beginTransaction();
+        hideAllFragment(fTransaction);
+        switch (checkedId){
+            case R.id.rb_home:
+                if(fg1 == null){
+                    fg1 = new FragmentHome();
+                    fTransaction.add(R.id.ly_content,fg1);
+                }else{
+                    fTransaction.show(fg1);
+                }
+                break;
+            case R.id.rb_contact:
+                if(fg2 == null){
+                    fg2 = new FragmentContact();
+                    fTransaction.add(R.id.ly_content,fg2);
+                }else{
+                    fTransaction.show(fg2);
+                }
+                break;
+            case R.id.rb_my:
+                if(fg3 == null){
+                    fg3 = new FragmentMy();
+                    fTransaction.add(R.id.ly_content,fg3);
+                }else{
+                    fTransaction.show(fg3);
+                }
+                break;
+        }
+        fTransaction.commit();
+    }
+
+    //隐藏所有Fragment
+    private void hideAllFragment(FragmentTransaction fragmentTransaction){
+        if(fg1 != null)fragmentTransaction.hide(fg1);
+        if(fg2 != null)fragmentTransaction.hide(fg2);
+        if(fg3 != null)fragmentTransaction.hide(fg3);
+    }
+
 }
